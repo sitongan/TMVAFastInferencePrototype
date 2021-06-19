0a1d2341f218f7b30bc29fd6839b34768476d285
axmat <amthamdan@gmail.com>
2021-06-20 00:13:19 +0200

    Remove Op Conv from this PR

--- RModelParser_ONNX.hxx
+++ RModelParser_ONNX.hxx
@@ -27,7 +27,6 @@ namespace INTERNAL{
 std::unique_ptr<ROperator> make_ROperator_Transpose(const onnx::NodeProto& nodeproto, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
 std::unique_ptr<ROperator> make_ROperator_Relu(const onnx::NodeProto& nodeproto, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
 std::unique_ptr<ROperator> make_ROperator_Gemm(const onnx::NodeProto& nodeproto, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
-std::unique_ptr<ROperator> make_ROperator_Conv(const onnx::NodeProto& nodeproto, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
 std::unique_ptr<ROperator> make_ROperator_RNN(const onnx::NodeProto& nodeproto, const onnx::GraphProto& graphproto, std::unordered_map<std::string, ETensorType>& tensor_type);
 
 using factoryMethodMap = std::unordered_map<std::string, std::unique_ptr<ROperator> (*)(const onnx::NodeProto&, const onnx::GraphProto&, std::unordered_map<std::string, ETensorType>&)>;
@@ -35,7 +34,6 @@ const factoryMethodMap mapOptypeOperator = {
       {"Gemm", &make_ROperator_Gemm},
       {"Transpose", &make_ROperator_Transpose},
       {"Relu", &make_ROperator_Relu},
-      {"Conv", &make_ROperator_Conv},
       {"RNN", &make_ROperator_RNN}
    };
 
