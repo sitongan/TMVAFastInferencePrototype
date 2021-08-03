bf2b0ef3d66f2f962447fc543bfe4a101fc3b126
AaradhyaSaxena <aaradhya271197@gmail.com>
2021-08-03 22:08:40 +0530

    updated generated file

--- testCaseBatchNorm_1.hxx
+++ testCaseBatchNorm_1.hxx
@@ -1,4 +1,4 @@
-//Code generated automatically by TMVA for Inference of Model file [testCaseBatchNorm_1.onnx] at [Tue Aug  3 15:13:55 2021] 
+//Code generated automatically by TMVA for Inference of Model file [testCaseBatchNorm_1.onnx] at [Tue Aug  3 16:38:04 2021] 
 #include<cmath>
 #include<vector>
 namespace TMVA_SOFIE_testCaseBatchNorm_1{
@@ -18,15 +18,43 @@ float tensor_bias[3] = {-0.844192743, -1.79882622, -0.302479535};
 float tensor_s[3] = {2.34357285, 0.356457889, 1.21757233};
 float tensor_y[120];
 std::vector<float> infer(float* tensor_x){
-	for (size_t n = 0; n < 2; n++) {
-		for (size_t c = 0; c < 3; c++) {
-			for (size_t h = 0; h < 4; h++) {
-				for (size_t w = 0; w < 5; w++) {
-					tensor_y[n * 60 + c * 20 + h * 5 + w] = ((tensor_x[n * 60 + c * 20 + h * 5 + w] - tensor_mean[c])/ std::sqrt(tensor_var[c] + 1e-05 ))* tensor_s[c] + tensor_bias[c];
-				}
+	float op_0_A[3];
+	for (size_t c = 0; c < 3; c++) {
+		op_0_A[c] = (tensor_s[c] / std::sqrt(tensor_var[c] + 1e-05 )); 
+	}
+	float op_0_Ba[120];
+	float op_0_Bmean[120];
+	size_t bs = 0;
+	for (size_t c = 0; c < 3; c++) {
+		for (size_t h = 0; h < 4; h++) {
+			for (size_t w = 0; w < 5; w++) {
+				op_0_Ba[ bs* 60 + c * 20 + h * 5 + w] = op_0_A[c];
+				op_0_Bmean[ bs* 60 + c * 20 + h * 5 + w] = tensor_mean[c];
+				tensor_y[ bs* 60 + c * 20 + h * 5 + w] = tensor_bias[c];
 			}
 		}
 	}
+	size_t op_0_batchOffset = 60;
+	for (bs = 0; bs < 2; bs++) {
+		std::copy(op_0_Ba, op_0_Ba+ op_0_batchOffset, op_0_Ba+ (bs* op_0_batchOffset));
+		std::copy(op_0_Bmean, op_0_Bmean+op_0_batchOffset, op_0_Bmean+ (bs*op_0_batchOffset));
+		std::copy(tensor_y, tensor_y +op_0_batchOffset, tensor_y + (bs*op_0_batchOffset));
+	}
+	float op_0_C[120];
+	std::copy(tensor_x, tensor_x+120, op_0_C);
+	const int N =120;
+	const int op_0_incx = 1;
+	const int op_0_incy = 1;
+	float op_0_alpha = -1;
+	BLAS::saxpy_(&N, &op_0_alpha, op_0_Bmean, &op_0_incx,op_0_C, &op_0_incy);
+
+	char op_0_uplo = 'L';
+	const int op_0_k = 0;
+	const int op_0_lda = 1;
+	float op_0_beta = -1;
+	op_0_alpha = 1;
+	BLAS::ssbmv_(&op_0_uplo, &N, &op_0_k, &op_0_alpha, op_0_C, &op_0_lda, op_0_Ba, &op_0_incx, &op_0_beta, tensor_y, &op_0_incy);
+
 	std::vector<float> ret (tensor_y, tensor_y + sizeof(tensor_y) / sizeof(tensor_y[0]));
 	return ret;
 }
